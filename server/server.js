ee791e91b9eff25cbe8b138e460c473fb1dfbca8
jeremyhu9 <jeremyhu415@gmail.com>
2015-12-01 21:01:14 -0800

    fixed pointer to bower_components

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(session({secret: secret}));
 app.use(express.static('app'));
 
-app.use('/bower_components', express.static(__dirname + '/bower_components'));
+app.use('/bower_components', express.static(__dirname + 'app/bower_components'));
 
 var port = process.env.PORT || 8080;
 
